da3702bdc2da6d411275aa271ebd63e1142dcf5a
Matt Campbell <mattandhisapple@gmail.com>
2017-03-19 23:22:39 -0500

    Fix Broken Modal Link

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -7,15 +7,12 @@ $(document).ready(function() {
     else {
         navHeight = 120;
     }
-    if ($('a[href^="#"]').hasClass( "modal" ) ) {
 
-    } else {
-        $( 'a[href^="#"]' ).click(function() {
-            var target = $(this.hash);
+    $( 'a[href^="#"]:not(.modal-link)' ).click(function() {
+        var target = $(this.hash);
             if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
             if (target.length == 0) target = $('html');
             $('html, body').animate({ scrollTop: target.offset().top-navHeight }, 1000);
             return false;
-        });
-    }
+    });
 });
