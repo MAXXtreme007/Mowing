491a5c9619838fd2b0e2a3fea07a3458d85b6af0
Matt Campbell <mattandhisapple@gmail.com>
2017-03-22 00:32:02 -0500

    Close Mobile Navbar on Link
    Navbar now closes when clicking on links.

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -8,11 +8,22 @@ $(document).ready(function() {
         navHeight = 120;
     }
 
-    $( 'a[href^="#"]:not(.modal-link)' ).click(function() {
-        var target = $(this.hash);
-            if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
-            if (target.length == 0) target = $('html');
-            $('html, body').animate({ scrollTop: target.offset().top-navHeight }, 1000);
-            return false;
-    });
+    $( 'a[href^="#"]' ).click(function() {
+        // only do this if navigation is visible, otherwise you see jump in navigation while collapse() is called
+        if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible") ) {
+            $('.navbar-collapse').collapse('toggle');
+        }
+});
+
+$( 'a[href^="#"]:not(.modal-link)' ).click(function() {
+    // only do this if navigation is visible, otherwise you see jump in navigation while collapse() is called
+    if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible") ) {
+        $('.navbar-collapse').collapse('toggle');
+    }
+    var target = $(this.hash);
+    if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+    if (target.length == 0) target = $('html');
+    $('html, body').animate({ scrollTop: target.offset().top-navHeight }, 1000);
+    return false;
+});
 });
